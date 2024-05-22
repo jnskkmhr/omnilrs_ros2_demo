547d7140974033861e7165b9d314ef699bf51de7
jnskkmhr <junjun.k2012@gmail.com>
2024-05-22 19:36:34 +0900

    support reconnection in micro ros

--- mros-arduino/micro_ros_subscriber_throttle_arr/micro_ros_subscriber_throttle_arr.ino
+++ mros-arduino/micro_ros_subscriber_throttle_arr/micro_ros_subscriber_throttle_arr.ino
@@ -6,6 +6,7 @@
 #include <rcl/error_handling.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
+#include <rmw_microros/rmw_microros.h>
 
 #include <geometry_msgs/msg/twist.h>
 #include <std_msgs/msg/float32_multi_array.h>
@@ -16,6 +17,7 @@ rcl_allocator_t allocator;
 rclc_support_t support;
 rcl_node_t node;
 std_msgs__msg__Float32MultiArray msg_float32arr;
+bool micro_ros_init_successful;
 
 #define LED_PIN 13
 
@@ -31,6 +33,18 @@ std_msgs__msg__Float32MultiArray msg_float32arr;
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define EXECUTE_EVERY_N_MS(MS, X)  do { \
+  static volatile int64_t init = -1; \
+  if (init == -1) { init = uxr_millis();} \
+  if (uxr_millis() - init > MS) { X; init = uxr_millis();} \
+} while (0)\
+
+enum states {
+  WAITING_AGENT,
+  AGENT_AVAILABLE,
+  AGENT_CONNECTED,
+  AGENT_DISCONNECTED
+} state;
 
 void error_loop(){
   while(1){
@@ -87,25 +101,7 @@ void subscription_callback(const void *msgin) {
   dacWrite(ANALOG_PIN_R, duty_r);
 }
 
-void setup() {
-  // serial communication
-  set_microros_transports();
-
-  // error handling LED pin setup
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, HIGH);
-
-  // motor driver pin setting
-  pinMode(CW_PIN_L, OUTPUT);
-  pinMode(CCW_PIN_L, OUTPUT);
-
-  pinMode(CW_PIN_R, OUTPUT);
-  pinMode(CCW_PIN_R, OUTPUT);
-
-  // DAC setting
-  pinMode(ANALOG_PIN_L, OUTPUT);
-  pinMode(ANALOG_PIN_R, OUTPUT);
-
+bool create_entities(){
   // memory allocator
   allocator = rcl_get_default_allocator();
 
@@ -123,17 +119,79 @@ void setup() {
     "throttle"));
 
   // create executor
+  executor = rclc_executor_get_zero_initialized_executor();
   RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
   RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg_float32arr, &subscription_callback, ON_NEW_DATA));
+  return true; 
+}
+
+void destroy_entities()
+{
+  rmw_context_t * rmw_context = rcl_context_get_rmw_context(&support.context);
+  (void) rmw_uros_set_context_entity_destroy_session_timeout(rmw_context, 0);
+  
+  rcl_subscription_fini(&subscriber, &node);
+  rclc_executor_fini(&executor);
+  rcl_node_fini(&node);
+  rclc_support_fini(&support);
+}
+
+void setup() {
+  // serial communication
+  set_microros_transports();
+
+  // error handling LED pin setup
+  pinMode(LED_PIN, OUTPUT);
+
+  // motor driver pin setting
+  pinMode(CW_PIN_L, OUTPUT);
+  pinMode(CCW_PIN_L, OUTPUT);
+
+  pinMode(CW_PIN_R, OUTPUT);
+  pinMode(CCW_PIN_R, OUTPUT);
+
+  // DAC setting
+  pinMode(ANALOG_PIN_L, OUTPUT);
+  pinMode(ANALOG_PIN_R, OUTPUT);
 
   // allocate memory
   // msg_float32arr has two float32 array (so, 2*4=8byte)
   msg_float32arr.data.data = (float *)malloc(2 * sizeof(float));
   msg_float32arr.data.size = 0;
   msg_float32arr.data.capacity = 4;
+
+  state = WAITING_AGENT;
 }
 
 void loop() {
-  RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
-  // delay(10);
+  switch (state) {
+    case WAITING_AGENT:
+      EXECUTE_EVERY_N_MS(500, state = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_AVAILABLE : WAITING_AGENT;);
+      break;
+    case AGENT_AVAILABLE:
+      state = (true == create_entities()) ? AGENT_CONNECTED : WAITING_AGENT;
+      if (state == WAITING_AGENT) {
+        destroy_entities();
+      };
+      break;
+    case AGENT_CONNECTED:
+      EXECUTE_EVERY_N_MS(200, state = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_CONNECTED : AGENT_DISCONNECTED;);
+      if (state == AGENT_CONNECTED) {
+        RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+      }
+      break;
+    case AGENT_DISCONNECTED:
+      destroy_entities();
+      state = WAITING_AGENT;
+      break;
+    default:
+      break;
+  }
+
+  if (state == AGENT_CONNECTED){
+    digitalWrite(LED_PIN, HIGH);
+  }
+  else {
+    digitalWrite(LED_PIN, LOW);
+  }
 }
