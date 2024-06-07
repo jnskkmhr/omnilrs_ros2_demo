ddf7839996e12f45740d4841f18e093dabc3bf59
jnskkmhr <junjun.k2012@gmail.com>
2024-06-07 12:44:19 +0900

    fix docker file

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -2,12 +2,13 @@
 xhost +
 IMAGE="moonraker:v1.0"
 NAME="moonraker-container"
-docker run --name $NAME -it --rm \
--e "ACCEPT_EULA=Y" --privileged \
+docker run -it --rm --privileged \
+-e "ACCEPT_EULA=Y" \
 -e DISPLAY \
 -e "PRIVACY_CONSENT=Y" \
---net host \
 -v $HOME/.Xauthority:/root/.Xauthority \
 -v /dev/:/dev/ \
--v $HOME/ros2_ws:/ros2_ws \ 
+-v $HOME/ros2_ws:/ros2_ws \
+--net host \
+--name $NAME \
 $IMAGE
