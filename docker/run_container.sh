950a44faee7283e09a75c071344ce8c5c20cdcfa
jnskkmhr <junjun.k2012@gmail.com>
2024-06-10 14:18:35 +0900

    change code structure

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -3,13 +3,21 @@ xhost +
 TASK=$1
 IMAGE="moonraker-${TASK}:v1.0"
 NAME="moonraker-${TASK}-container"
+
+if [${TAG} = "perception"]; then
+    ROS_VERSION="foxy"
+elif [${TAG} = "navigation"]; then
+    ROS_VERSION="humble"
+else
+    echo "WRONG TAG"
+    exit 0
+
 docker run -it --rm --privileged \
 -e "ACCEPT_EULA=Y" \
 -e DISPLAY \
 -e "PRIVACY_CONSENT=Y" \
 -v $HOME/.Xauthority:/root/.Xauthority \
 -v /dev/:/dev/ \
--v $HOME/ros2_ws:/ros2_ws \
+-v $HOME/rover_moonraker/${ROS_VERSION}_ws:/ros2_ws \
 --name $NAME \
---net host \
 $IMAGE
