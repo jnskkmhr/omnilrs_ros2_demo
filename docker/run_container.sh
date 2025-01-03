3d3523dec040f752741960bcf7d91207e3cab979
Tharit Sinsunthorn <tharit.sinsunthorn.r1@dc.tohoku.ac.jp>
2025-01-03 15:55:42 +0900

    fix gpu run time problem for rerun

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -3,9 +3,14 @@ xhost +local:root
 IMAGE="omnilrs-navigation:v1.0"
 NAME="omnilrs-navigation-container"
 DOCKER_RUN_CMD="docker run -it --rm --privileged \
+                --gpus all \
+                --runtime=nvidia \
+                -e NVIDIA_VISIBLE_DEVICES=all \
+                -e NVIDIA_DRIVER_CAPABILITIES=all \
+                -e XDG_RUNTIME_DIR=/tmp/runtime-docker \
                 -e "ACCEPT_EULA=Y" \
                 -e DISPLAY \
-                -e "PRIVACY_CONSENT=Y" \
+                -e "PRIVACY_CONSENT=Y"                 
                 -v $HOME/.Xauthority:/root/.Xauthority \
                 -v /dev/:/dev/
                 -v $PWD/docker:/docker \
@@ -14,5 +19,8 @@ DOCKER_RUN_CMD="docker run -it --rm --privileged \
                 --ipc=host \
                 --name $NAME \
                 $IMAGE"
+
+# Create runtime directory
+mkdir -p /tmp/runtime-docker
 
 exec ${DOCKER_RUN_CMD[*]}
